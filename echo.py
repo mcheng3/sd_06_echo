d8c35632f31111f1afef54871be0012e1081e9c3
mcheng3 <michael.cheng65@gmail.com>
2017-10-02 20:57:02 -0400

    Finish echo.py and input.html

--- echo.py
+++ echo.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('form.html')
+	return render_template('form.html', title = 'Main')
 
-@app.route('/welcomePage', methods=['POST'])
+@app.route('/welcomePage', methods=['POST','GET'])
 def welcome():
-	return render_template('input.html')
+	print request.headers
+	print request.method
+	print request.form
+	print request.form['user']
+	return render_template('input.html', title = request.form['user'] + "'s", user = request.form['user'], method = request.method)
 
 
 if __name__ == "__main__":
