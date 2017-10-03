e08c0238075a9e4940f1298d310966e38557473c
mcheng3 <michael.cheng65@gmail.com>
2017-10-02 20:37:47 -0400

    Complete form.html

--- echo.py
+++ echo.py
@@ -1,1 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('form.html')
+
+@app.route('/welcomePage', methods=['POST'])
+def welcome():
+	return render_template('input.html')
+
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
